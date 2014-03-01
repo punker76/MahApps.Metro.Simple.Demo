a58dde3ab00ed6f37bc783e0e409ec26b2edfaf6
Jan Karger <punkerat76@gmail.com>
2014-03-01 19:53:09 +0100

    the example itself

---
+++ MahApps.Metro.Simple.Demo/MahApps.Metro.Simple.Demo/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MahApps.Metro.Controls;
+
+namespace MahApps.Metro.Simple.Demo
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : MetroWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
